663f1057413dea3bd3364ad36ad8f59c9e4abcce
levym <ducksoupdev@gmail.com>
2015-06-23 12:29:35 +0100

    Update client test.

--- client-superagent.js
+++ client-superagent.js
@@ -1,27 +1,59 @@
 var request = require('superagent');
 
-var server = 'http://172.26.53.116';
+// var server = 'http://172.26.53.116';
+var server = 'http://localhost';
 
 var games = [
-    { method: 'post', url: '/start', params: { opponentName: 'Bob' } },
-    { method: 'post', url: '', params: {} },
-    { method: 'get', url: '', params: '' },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'PAPER' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'PAPER' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'PAPER' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'PAPER' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'PAPER' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'PAPER' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'PAPER' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
+    { method: 'post', url: '/move', params: { lastOpponentMove: 'ROCK' } },
+    { method: 'get', url: '/move', params: null, expects: 'ROCK' },
 ];
+
+request
+    .post(server + '/start')
+    .type('form')
+    .send({
+        opponentName: 'Bob',
+        pointsToWin: 1000,
+        maxRounds: 1000,
+        dynamiteCount: 3
+    })
+    .end(function (err, res) {
+        // console.log(res.text);
+    });
+
 
 games.forEach(function (game) {
     if (game.method === 'post') {
+        
         request
             .post(server + game.url)
             .type('form')
             .send(game.params)
             .end(function (err, res) {
-                console.log(res.text);
-            });
+            // console.log(res.text);
+        });
     } else if (game.method === 'get') {
         request
-            .get(server + game.url + '?' + game.params)
+            .get(server + game.url)
             .end(function (err, res) {
-                console.log(res.text);
-            });
+            console.log('opponent: ' + game.params.lastOpponentMove);
+            console.log(res.text);
+            console.log(res.text + ' equals ' + game.expects + ': ' + res.text === game.expects);
+        });
     }
 });
