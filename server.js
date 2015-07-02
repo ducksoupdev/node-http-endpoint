543e8805e88b2583572cfe44957ef0ef99abdc9f
luke88jones <luke@jonesbc.co.uk>
2015-07-02 22:22:28 +0100

    Final changes.

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ function Game(name, pointsToWin, maxRounds, dynamiteCount) {
     this.dynamiteCount = dynamiteCount;
     this.moves = [];
     this.ourMoves = [];
+    this.playCount = {
+        "ROCK": 0,
+        "SCISSORS": 0,
+        "PAPER": 0,
+        "DYNAMITE": 0,
+        "WATERBOMB": 0
+    };
+    this.doesDoubleDyno = false;
 }
 
 var formatRequest = function(body) {
@@ -28,7 +36,7 @@ var winners = {
     'SCISSORS': 'ROCK',
     'DYNAMITE': 'WATERBOMB',
     'WATERBOMB': 'ROCK'
-}
+};
 
 var isLastFiveTheSame = function() {
     if (currentGame.moves.length < 6) {
@@ -46,8 +54,25 @@ var isLastFiveTheSame = function() {
     return null;
 };
 
+var getMostFrequentWinner = function() {
+    var mostFrequent = null;
+    for (var key in currentGame.playCount) {
+        if (key !== "DYNAMITE" && key !== "WATERBOMB") {
+            if (!mostFrequent) {
+                mostFrequent = key;
+            }
+            if (currentGame.playCount[key] > currentGame.playCount[mostFrequent]) {
+                mostFrequent = key;
+            }    
+        }
+    }
+    console.log("Most Frequent");
+    console.log(mostFrequent);
+    return winners[mostFrequent];
+};
+
 var getMove = function() {
-    var move = "ROCK";
+    var move = "SCISSORS";
     var gameLength;
     if (currentGame) {
         gameLength = currentGame.moves.length;    
@@ -65,23 +90,42 @@ var getMove = function() {
             return lastMove;
         }
         
-        if (currentGame.moves[gameLength -1] === currentGame.ourMoves[currentGame.ourMoves.length - 1] &&
-            currentGame.dynamiteCount > 0) {
-            move = 'DYNAMITE';
-            currentGame.dynamiteCount--;
+        if (currentGame.moves[gameLength -1] === currentGame.ourMoves[currentGame.ourMoves.length - 1]) {
+            if (ourLast === "DYNAMITE") {
+                if (currentGame.doesDoubleDyno) {
+                    move = 'WATERBOMB'    
+                }
+                else {
+                    move = getMostFrequentWinner();
+                }
+                
+            }
+            else if (currentGame.dynamiteCount > 0) {
+                move = 'DYNAMITE';
+                currentGame.dynamiteCount--;    
+            } else {
+                move = winners[ourLast];
+            }
+
+            
         } else if (gameLength > 1 && 
                     currentGame.moves[gameLength -1] === "DYNAMITE" &&
                     currentGame.moves[gameLength -2] === "DYNAMITE" &&
                     currentGame.moves[gameLength -3] === "DYNAMITE") {
             move = 'WATERBOMB';
+        } else {
+            move = getMostFrequentWinner();    
         }
+        
     }
  
     return move;
 };
 
 var currentGame = null,
-    roundCount = 0;
+    roundCount = 0,
+    ourLast = null,
+    theirLast = null;
 
 var server = http.createServer( function(req, res) {
     if (req.method == 'POST') {
@@ -107,6 +151,11 @@ var server = http.createServer( function(req, res) {
             else if (/move/.test(req.url)) {
                 console.log('Opponent Move');
                 console.log(data.lastOpponentMove);
+                if (theirLast === "DYNAMITE" && data.lastOpponentMove === "DYNAMITE") {
+                    currentGame.doesDoubleDyno = true;
+                }
+                currentGame.playCount[data.lastOpponentMove]++;
+                theirLast = data.lastOpponentMove;
                 currentGame.moves.push(data.lastOpponentMove);
             }
 
@@ -121,6 +170,7 @@ var server = http.createServer( function(req, res) {
             res.writeHead(200, {'Content-Type': 'text/plain'});
             var move = getMove();
             currentGame.ourMoves.push(move);
+            ourLast = move;
             console.log('Move: ' + move);
 
             res.end(move);
